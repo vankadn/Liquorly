791097f406615beab7b67421f07adada8995e624
Krishna <Krishna.Vankadari@wolterskluwer.com>
2016-12-25 16:19:04 -0500

    - Test

---
+++ Vidly/Controllers/BottlesController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using static System.String;
+
+namespace Vidly.Controllers
+{
+    public class BottlesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Bottle() {Name = "Chivas Regal"};
+
+            //            return View(movie);
+            //            return Content("Doood");
+            //            return HttpNotFound();
+            //            return new EmptyResult();
+            return RedirectToAction("Index", "Home", new {page=1, sortBy = "name"});
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id :" + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+            if (IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+            return Content($"pageIndex={pageIndex}& sortBy={sortBy}");
+        }
+    }
+}   
